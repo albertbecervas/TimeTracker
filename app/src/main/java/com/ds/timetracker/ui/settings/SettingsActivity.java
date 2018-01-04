e2121a26147ebadec9f0916c3987bfd64221999a
arnauochoa <arnau.ochoa@gmail.com>
2018-01-04 19:14:24 +0100

    falta arreglar lo dels idiomes

--- app/src/main/java/com/ds/timetracker/ui/settings/SettingsActivity.java
+++ app/src/main/java/com/ds/timetracker/ui/settings/SettingsActivity.java
@@ -1,45 +1,67 @@
 package com.ds.timetracker.ui.settings;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.ds.timetracker.R;
 import com.ds.timetracker.ui.MainActivity;
 import com.ds.timetracker.ui.create.SpinnerAdapter;
 import com.ds.timetracker.utils.AppSharedPreferences;
 
+import java.util.Locale;
+
 public class SettingsActivity extends AppCompatActivity {
 
     private AppSharedPreferences mPrefs;
 
+    private TextView titleLanguage;
+    private TextView titleClock;
+    Spinner languagePicker;
+    SpinnerAdapter languageAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        onCreateMethod();
+
+    }
+
+    private void onCreateMethod() {
+
         setContentView(R.layout.activity_settings);
 
         mPrefs = AppSharedPreferences.getInstance(this);
 
         setViews();
+    }
 
+
+    private void setLocale(Locale locale) {
+        Locale.setDefault(locale);
+        Configuration config = getBaseContext().getResources().getConfiguration();
+        config.locale = locale;
+        getBaseContext().getResources().updateConfiguration(config,
+                getBaseContext().getResources().getDisplayMetrics());
     }
 
     private void setViews() {
-        final String[] languagesNames = {getString(R.string.english), getString(R.string.catalan), getString(R.string.spanish)};
-        final int languages[] = {R.drawable.usa, R.drawable.catalunya, R.drawable.spain};
 
         if (getSupportActionBar() != null) {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
 
+        titleLanguage = findViewById(R.id.title);
+        titleClock = findViewById(R.id.title_clock);
+
         //spinner that sets the Locale of the phone in order to change the language
-        Spinner languagePicker = findViewById(R.id.spinner);
-        SpinnerAdapter languageAdapter = new SpinnerAdapter(this, languages, languagesNames);
-        languagePicker.setAdapter(languageAdapter);
+        setSpinnerAdapter();
         languagePicker.setSelection(getPosition());
         languagePicker.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
@@ -65,6 +87,14 @@ public class SettingsActivity extends AppCompatActivity {
 
                 mPrefs.setLocale(locale);
                 mPrefs.set24HFormat(is24h);
+
+                /*setLocale(new Locale(mPrefs.getLocale()));
+
+                setSpinnerAdapter();
+
+                titleLanguage.setText(getString(R.string.language));
+                titleClock.setText(getString(R.string.minimum_clock_time));*/
+
             }
 
             @Override
@@ -72,6 +102,15 @@ public class SettingsActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    private void setSpinnerAdapter() {
+
+        final String[] languagesNames = {getString(R.string.english), getString(R.string.catalan), getString(R.string.spanish)};
+        final int languages[] = {R.drawable.usa, R.drawable.catalunya, R.drawable.spain};
+        languagePicker = findViewById(R.id.spinner);
+        languageAdapter = new SpinnerAdapter(this, languages, languagesNames);
+        languagePicker.setAdapter(languageAdapter);
     }
 
     private int getPosition() {
